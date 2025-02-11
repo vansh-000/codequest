3c0610727249dbc954e03881f85b3e65d8d5fc39
vansh-000 <23165@iiitu.ac.in>
2025-02-11 15:44:06 +0530

    initialize Next.js project

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <main className="bg-dark-layer-2 min-h-screen">
+        <h1
+          className="text-2xl text-center text-gray-700 dark:text-gray-400 font-medium
+					uppercase mt-10 mb-5"
+        >
+          &ldquo; QUALITY OVER QUANTITY &rdquo; 👇
+        </h1>
+      </main>
+    </>
+  );
+}
