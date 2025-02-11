627c719cea6959b2bb39f170fb74bc0acf878da3
vansh-000 <23165@iiitu.ac.in>
2025-02-12 00:31:43 +0530

    implement login functionality

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../UI/Button";
 import { useSetRecoilState } from "recoil";
 import { authModalState } from "@/atoms/authModalAtom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
+import { auth } from "@/firebase/firebase";
 
 type LoginProps = {};
 
@@ -10,9 +14,36 @@ const Login: React.FC<LoginProps> = () => {
 	const handleClick = (type: "login" | "register" | "forgotPassword") => {
 		setAuthModalState((prev) => ({ ...prev, type }));
 	};
+	const [inputs, setInputs] = useState({ email: "", password: "" });
+	const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+	const router = useRouter();
+	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	};
+	const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		if (!inputs.email || !inputs.password) return alert("Please fill all fields");
+		try {
+			const newUser = await signInWithEmailAndPassword(inputs.email, inputs.password);
+			if (!newUser) return;
+			router.push("/");
+		} catch (error: any) {
+			toast.error(error.message, { position: "top-center", autoClose: 3000, theme: "dark" });
+		}
+	};
+	useEffect(() => {
+		if (error) {
+			if (error.message == "Firebase: Error (auth/invalid-credential).") {
+				toast.error('Invalid Credentials')
+			}
+			else {
+				toast.error(error.message);
+			}
+		};
+	}, [error]);
 
 	return (
-		<form className="space-y-4 pb-2 max-w-md mx-auto">
+		<form onSubmit={handleLogin} className="space-y-4 pb-2 max-w-md mx-auto">
 			<div className="flex justify-center">
 				<h3 className="inline-flex items-center text-2xl md:text-3xl font-bold text-white">
 					Login
@@ -28,6 +59,7 @@ const Login: React.FC<LoginProps> = () => {
 				</label>
 				<input
 					type="email"
+					onChange={handleInputChange}
 					name="email"
 					id="email"
 					className="border-2 outline-none sm:text-sm md:text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
@@ -44,6 +76,7 @@ const Login: React.FC<LoginProps> = () => {
 				</label>
 				<input
 					type="password"
+					onChange={handleInputChange}
 					name="password"
 					id="password"
 					className="border-2 outline-none sm:text-sm md:text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"
